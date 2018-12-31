0f70748d3febc5622405f6f8086337cf6c325954
FireBenderRyan <cooltardis995@gmail.com>
2018-12-30 19:09:41 -0500

    Update function-review.js

--- function-review.js
+++ function-review.js
@@ -1,22 +1,17 @@
 //Read questions in the README and answer them here
 
 //Q.1 code here
-//one is creating and the other is invoking 
-//creating
+ 
 var Function1 = function(){
 
 }
-
-Function1();
 
 function Function1(){
 
 }
 
-Function1();
-
 //Q.2 code here
-
+//one is creating and the other is invoking
 //Definition
 
 var Function1(){
@@ -30,16 +25,14 @@ Function1();
 
 function Function1(argument){
 
- (argument + 3)
+ return(argument + 3)
 }
 
-Function1();
+Function1(3); // -> 6
 
 //Q.4 code here
 
 function function2(){
-
-var one = 1
-
-return (one + 1)	
+   var one = 1
+   return (one + 1)
 }
